3f1f3aa1b74a8104452367f9af422c49600ad4db
LucaNoah <luca.gfeller2@gmail.com>
2022-05-12 13:31:33 +0000

    In new_game function add entries to dictionary to improve positioning of fields for the user. Change them also where they are called in print_boards function.

--- run.py
+++ run.py
@@ -15,11 +15,12 @@ def new_game():
   print("RULES, blbalablalbalbalablablabalballbalbalba")
 
   board_player = {
-  "A1": ".", "A2": ".", "A3": ".", "A4": ".", "A5": ".",
-  "B1": ".", "B2": ".", "B3": ".", "B4": ".", "B5": ".",
-  "C1": ".", "C2": ".", "C3": ".", "C4": ".", "C5": ".",
-  "D1": ".", "D2": ".", "D3": ".", "D4": ".", "D5": ".",
-  "E1": ".", "E2": ".", "E3": ".", "E4": ".", "E5": ".",
+  "f0": "*", "fA": "A", "fB": "B", "fC": "C", "fD": "D", "fE": "E",
+  "f1": "1", "A1": ".", "B1": ".", "C1": ".", "D1": ".", "E1": ".",
+  "f2": "2", "A2": ".", "B2": ".", "C2": ".", "D2": ".", "E2": ".",
+  "f3": "3", "A3": ".", "B3": ".", "C3": ".", "D3": ".", "E3": ".",
+  "f4": "4", "A4": ".", "B4": ".", "C4": ".", "D4": ".", "E4": ".",
+  "f5": "5", "A5": ".", "B5": ".", "C5": ".", "D5": ".", "E5": ".",
   }
 
   board_computer = copy.copy(board_player)
@@ -59,11 +60,12 @@ def print_boards(name, board1, board2):
 
   print(name)
 
-  print(board1["A1"], board1["A2"], board1["A3"], board1["A4"], board1["A5"])
-  print(board1["B1"], board1["B2"], board1["B3"], board1["B4"], board1["B5"])
-  print(board1["C1"], board1["C2"], board1["C3"], board1["C4"], board1["C5"])
-  print(board1["D1"], board1["D2"], board1["D3"], board1["D4"], board1["D5"])
-  print(board1["E1"], board1["E2"], board1["E3"], board1["E4"], board1["E5"])
+  print(board1["f0"], board1["fA"], board1["fB"], board1["fC"], board1["fD"], board1["fE"])
+  print(board1["f1"], board1["A1"], board1["B1"], board1["C1"], board1["D1"], board1["E1"])
+  print(board1["f2"], board1["A2"], board1["B2"], board1["C2"], board1["D2"], board1["E2"])
+  print(board1["f3"], board1["A3"], board1["B3"], board1["C3"], board1["D3"], board1["E3"])
+  print(board1["f4"], board1["A4"], board1["B4"], board1["C4"], board1["D4"], board1["E4"])
+  print(board1["f5"], board1["A5"], board1["B5"], board1["C5"], board1["D5"], board1["E5"])
 
   for k, v in board2.items():
     if v == "@":
@@ -71,11 +73,12 @@ def print_boards(name, board1, board2):
 
   print("Computer")
 
-  print(board2["A1"], board2["A2"], board2["A3"], board2["A4"], board2["A5"])
-  print(board2["B1"], board2["B2"], board2["B3"], board2["B4"], board2["B5"])
-  print(board2["C1"], board2["C2"], board2["C3"], board2["C4"], board2["C5"])
-  print(board2["D1"], board2["D2"], board2["D3"], board2["D4"], board2["D5"])
-  print(board2["E1"], board2["E2"], board2["E3"], board2["E4"], board2["E5"])
+  print(board2["f0"], board2["fA"], board2["fB"], board2["fC"], board2["fD"], board2["fE"])
+  print(board2["f1"], board2["A1"], board2["B1"], board2["C1"], board2["D1"], board2["E1"])
+  print(board2["f2"], board2["A2"], board2["B2"], board2["C2"], board2["D2"], board2["E2"])
+  print(board2["f3"], board2["A3"], board2["B3"], board2["C3"], board2["D3"], board2["E3"])
+  print(board2["f4"], board2["A4"], board2["B4"], board2["C4"], board2["D4"], board2["E4"])
+  print(board2["f5"], board2["A5"], board2["B5"], board2["C5"], board2["D5"], board2["E5"])
 
 
 def main():
@@ -84,5 +87,3 @@ def main():
 
 main()
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
-
-
