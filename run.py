6b2f1267b6960455820cf1922ee9fd87758c9b93
LucaNoah <luca.gfeller2@gmail.com>
2022-05-12 10:19:57 +0000

    Define print_boards function to print both boards, swap the ships with a dot on the board of the computer so that the player does not see them. Call the function in the new_game function with player name and the 2 boards as arguments

--- run.py
+++ run.py
@@ -25,6 +25,7 @@ def new_game():
   board_computer = copy.copy(board_player)
 
   place_ships(board_player, board_computer)
+  print_boards(name_player, board_player, board_computer)
 
   return name_player, board_player, board_computer
 
@@ -51,12 +52,37 @@ def place_ships(board1, board2):
   board2[ships_computer[3]] = "@"
 
 
+def print_boards(name, board1, board2):
+  """
+  Displays the two game boards, the one of the computer does not show the ships.
+  """
+
+  print(name)
+
+  print(board1["A1"], board1["A2"], board1["A3"], board1["A4"], board1["A5"])
+  print(board1["B1"], board1["B2"], board1["B3"], board1["B4"], board1["B5"])
+  print(board1["C1"], board1["C2"], board1["C3"], board1["C4"], board1["C5"])
+  print(board1["D1"], board1["D2"], board1["D3"], board1["D4"], board1["D5"])
+  print(board1["E1"], board1["E2"], board1["E3"], board1["E4"], board1["E5"])
+
+  for k, v in board2.items():
+    if v == "@":
+      board2[k] = "."
+
+  print("Computer")
+
+  print(board2["A1"], board2["A2"], board2["A3"], board2["A4"], board2["A5"])
+  print(board2["B1"], board2["B2"], board2["B3"], board2["B4"], board2["B5"])
+  print(board2["C1"], board2["C2"], board2["C3"], board2["C4"], board2["C5"])
+  print(board2["D1"], board2["D2"], board2["D3"], board2["D4"], board2["D5"])
+  print(board2["E1"], board2["E2"], board2["E3"], board2["E4"], board2["E5"])
+
+
 def main():
   name_player, board_player, board_computer = new_game()
 
 
 main()
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
-
 
 
