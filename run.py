709baaeb4117a03e12ce4251f500a6181ac15d3c
LucaNoah <luca.gfeller2@gmail.com>
2022-05-12 09:54:19 +0000

    Define place_ships function to mark 4 ships with @ on both boards. Call the function in the new_game function with the 2 boards as arguments.

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+import random
 import copy
 
 def new_game():
@@ -23,5 +24,30 @@ def new_game():
 
   board_computer = copy.copy(board_player)
 
-  
+  place_ships(board_player, board_computer)
+
+
+def place_ships(board1, board2):
+  """
+  Places 4 random ships with an @ on both game boards.
+  """
+
+  list_fields = ["A1", "A2", "A3", "A4", "A5", "B1", "B2", "B3", "B4", "B5", "C1", "C2", "C3", "C4", "C5", "D1", "D2", "D3", "D4", "D5", "E1", "E2", "E3", "E4", "E5"]
+
+  ships_player = random.sample(list_fields, 4)
+
+  ships_computer = random.sample(list_fields, 4)
+
+  board1[ships_player[0]] = "@"
+  board1[ships_player[1]] = "@"
+  board1[ships_player[2]] = "@"
+  board1[ships_player[3]] = "@"
+
+  board2[ships_computer[0]] = "@"
+  board2[ships_computer[1]] = "@"
+  board2[ships_computer[2]] = "@"
+  board2[ships_computer[3]] = "@"
+
+
+new_game()
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
